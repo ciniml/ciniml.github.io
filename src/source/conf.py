6214df7ff6cf817bef822142891273716816864c
Kenta IDA <cinimlm@gmail.com>
2015-02-28 16:50:18 +0900

    Change the theme

--- src/source/conf.py
+++ src/source/conf.py
@@ -99,15 +99,20 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'defaultex'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 #.
-#html_theme_options = {}
+html_theme_options = {
+	"externalrefs": "true",
+	"sidebarbgcolor": "#404040",
+	"relbarbgcolor": "#404040",
+	"footerbgcolor": "#202020",
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['.']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release>".
